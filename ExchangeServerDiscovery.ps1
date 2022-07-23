97e38ad00560c792d2f817fe6e9a767bbfce1344
Andres Mariano Gorzelany <36666927+get-itips@users.noreply.github.com>
2022-07-23 13:41:50 +0000

    wip

---
+++ ExchangeServerDiscovery.ps1
@@ -0,0 +1,25 @@
+[CmdletBinding(DefaultParameterSetName = "ExchangeDiscovery", SupportsShouldProcess)]
+param(
+    [Parameter(Mandatory)]
+    [string]$Path,
+    )
+
+
+$Encoding="UTF8"
+
+#Import cmdlets CSV file
+$ExchangeServerCmdlets=Import-Csv -Delimiter ";" -Path ".\Cmdlets\ExchangeServer.csv"
+
+#Start
+foreach($ExchangeServerCmdlet in $ExchangeServerCmdlets)
+{   
+    Write-Verbose "Working on cmdlet $ExchangeServerCmdlet"
+    $filename=$ExchangeServerCmdlet.filename+".XML"
+    try{
+        (Invoke-Expression $ExchangeServerCmdlet.cmdlet) | Export-Clixml -Path "$Path\$filename" -Encoding $Encoding -Depth $ExchangeServerCmdlet.depth -Verbose
+    }
+    catch{
+        Write-Host "Failed to properly run $ExchangeServerCmdlet"
+    }
+
+}
